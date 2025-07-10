151523bb60b429a6207723f59577c99f9c7f2188
Alexis27000 <alexisseclin27@hotmail.fr>
2025-07-10 16:52:21 +0200

    bouton fermer

--- pages/scripttwo.js
+++ pages/scripttwo.js
@@ -74,6 +74,20 @@ fetch('../composants/footertwo.html')
   })
   .then(data => {
     footertwo.innerHTML = data;
+
+$('.popin').hide(); /*fadeOut()*/
+			$('footer nav ul li a').click(function(){
+				url = $(this).attr('href');
+				//alert(url);
+				$(url).show();/*fadeIn()*/
+				return false; /* Je n execute pas le lien */
+	
+			});
+			$('a.fermer').click(function(){
+				$('.popin').hide();/*fadeIn()*/
+				return false; /* Je n execute pas le lien */
+	
+			});
   })
   .catch(error => {
     console.error('Erreur :', error);
